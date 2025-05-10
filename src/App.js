9c8ae87c168462fdf11a71ac06a46f1a3b8e1a09
Swarasandy <skr996@gmail.com>
2025-05-10 11:28:01 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'; 
+import './App.css';
+import Nav from './componets/Nav';
+import Main from './componets/Main';
+import Menu from './componets/Menu';
+  return(
+  <>
+ <Nav/>
+ <Main/>
+ <Menu/>   
+</>  
+
+);   
+
+export default App;
---
+++ src/recpie.js
@@ -0,0 +1,27 @@
+const recpies =[
+  {
+    id: 1,
+    title: "Greek salad",
+    price:12.99,
+    image:"Greek salad.jpeg",
+    description:
+    "traditional Greek salad consists of sliced cucumbers, tomatoes, green bell pepper, red onion, olives, and feta cheese".
+
+},
+{
+  id:2,
+  title:"Bruchetta",
+  price:5.99,
+  image:"Bruchetta.jpeg",
+  description:
+  "In a medium bowl, mix tomatoes, garlic, basil, 2 tablespoons olive oil, balsamic vinegar, salt, and parmesan cheese".
+},   
+{
+    id: 3,
+    title: "Lemon Dessert",
+    price:4.78,
+    image:"Lemon Dessert.jpeg",
+    description:
+    "Pair your lemons with fruits like blueberries, raspberries and coconut to make anything from pies and cupcakes to pavlova."
+}
+]
